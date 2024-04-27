e8e7b622536ad79acec13ed3a161be86c830bc15
pankaj <pankaj_046@yahoo.in>
2024-04-27 23:28:58 +0530

    changing theme

--- src/component/NavbarContainer.jsx
+++ src/component/NavbarContainer.jsx
@@ -6,45 +6,33 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/navbar.css';
-import { FaPhone } from 'react-icons/fa'; 
-import { WiAlien } from "react-icons/wi";
 import { Outlet } from 'react-router-dom';
 import FooterContainer from './FooterContainer';
+import icon from '../assets/icon.svg';
+import Image from 'react-bootstrap/Image';
+
 
 const NavbarContainer = () => {
   return (
     <>
-    <Container className="container-full-height">
+    <Container className="container-full-height root-nav">
       <Row>
-      <Navbar expand="lg" className="bg-body-tertiary nav">
-        <Container>
-          <Navbar.Brand href="/" className="brandLogo">
-            B
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/" className="nav-link active">
-                Discover
-              </Nav.Link>
-              <Nav.Link href="/make-burger">Make Your Burger</Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end">
-              <Nav.Link href="#home">
-                <span className="callText navCall">
-                  <FaPhone className="callIcon flipped" /> Call Me Back
-                </span>
-              </Nav.Link>
-              <Nav.Link href="/order">Order</Nav.Link>
-              <Nav.Link href="/profile" className="nav-avator">
-                <div className="avatar-container">
-                  <WiAlien className="avator" />
-                </div>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar expand="lg" className="bg-body-tertiary nav" sticky="top">
+      <Container>
+        <Navbar.Brand href="/"><Image src={icon}></Image></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto"> 
+            <Nav.Link href="/" className="nav-link active">Home</Nav.Link>
+            <Nav.Link href="#" className="nav-link">Menu</Nav.Link> 
+            <Nav.Link href="#" className="nav-link">Cart</Nav.Link> 
+            <Nav.Link href="#" className="nav-link">Profile</Nav.Link> 
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
       </Row>
 
       <Row className="centerContainer">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,24 +4,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import leftBanner from '../assets/main-img.png';
 import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
 import './style/Home.css';
 
-import burger1 from '../assets/burger1.jpg';
-import burger2 from '../assets/burger2.jpg';
-import burger3 from '../assets/burger3.jpg';
-import burger4 from '../assets/burger4.jpg';
-
 const Home = () => {
-  const burgerTypes = [
-    { name: 'Classic Burger', type: 'Vegetarian', price: '$5.99', image: burger1 },
-    { name: 'Bacon Cheeseburger', type: 'Vegetarian', price: '$6.99', image: burger2 },
-    { name: 'Veggie Burger', type: 'Vegetarian', price: '$4.99', image: burger3 },
-    { name: 'Mushroom Swiss Burger', type: 'Vegetarian', price: '$7.99', image: burger4 },
-  ];
-
   return (
-    <Container>
+    <Container className="homeRoot">
       <Row>
         <Col xs={12} md={6} className="d-flex justify-content-center align-items-center flex-column">
           <Row>
@@ -31,27 +18,6 @@ const Home = () => {
         <Col xs={12} md={6} className="max-hed d-flex justify-content-center">
           <Image src={leftBanner} fluid />
         </Col>
-      </Row>
-      <Row className="burger-types justify-content-center mt-50">
-        <h1 center>
-        Hot Items
-        </h1>
-        {burgerTypes.map((burger, index) => (
-          <Col key={index} xs={12} md={6} lg={3} className="burger-type p-2">
-            <Card className="burger-card h-100">
-              <div className="img-container">
-                <Card.Img variant="top" src={burger.image} className="burger-img" />
-              </div>
-              <Card.Body>
-                <Card.Title>{burger.name}</Card.Title>
-                <Card.Text>
-                  <span className="burger-type">{burger.type}</span>
-                  <span className="burger-price">{burger.price}</span>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
       </Row>
     </Container>
   );
