3ae0415fd57f9de6cd1e5197ab4899ae2ffdb559
pankaj <pankaj_046@yahoo.in>
2024-04-28 18:22:09 +0530

    add contact home ui

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -6,9 +6,15 @@ import Button from 'react-bootstrap/Button';
 import './HomeStyle.css'
 import burger from '../../assets/burger_home.svg'
 import info_burger from '../../assets/info_burger.svg'
+import one from '../../assets/1.png'
+import two from '../../assets/2.png'
+import three from '../../assets/3.png'
+
+import delivery from '../../assets/delivery.svg'
+
 import Image from 'react-bootstrap/Image';
 import burgerList from './burgerList'; 
-
+import Card from 'react-bootstrap/Card';
 // import { BurgerDetails } from '../../component/burger-details/BurgerDetails'
 
 const HomePage = () => {
@@ -47,12 +53,66 @@ const HomePage = () => {
     </Container>
 
     <Container className="homeContact">
-    <Row className="align-items-center justify-content-center"> 
+    <Row className="justify-content-center"> 
         <Col xs={12} md={6} className="text-center"> 
+        <p className="mt-2 home-contact-title">Home</p>
+        <Image src={delivery} fluid/>
+          <Container>
+            <Row className="align-items-center justify-content-center">
+            <Card className="contact-card">
+              <Card.Body>
+                <Card.Title>Call us at:</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted card-subtitle">
+                  9988776655
+                </Card.Subtitle>
+              </Card.Body>
+            </Card>
+            
+            </Row>
+            <Row className="align-items-center justify-content-center">
+             
+            <Card className="contact-card">
+              <Card.Body>
+                <Card.Title>Write to us at:</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted card-subtitle">
+                  <span className="icon-contact">+91 889004455</span>
+                </Card.Subtitle>
+              </Card.Body>
+            </Card>
 
+            </Row>
+          </Container>
         </Col>
         <Col xs={12} md={6} className="text-center">
+        <p className="mt-2 home-contact-title">Restaurants</p>
       
+        <Card className="d-flex flex-row res-card">
+        <Card.Img variant="top" src={one} className="res-car-img" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+        <Card className="d-flex flex-row res-card">
+        <Card.Img variant="top" src={two} className="res-car-img" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+        <Card className="d-flex flex-row res-card">
+        <Card.Img variant="top" src={three} className="res-car-img" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
         </Col>
       </Row>
     </Container>
